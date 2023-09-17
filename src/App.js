24950e570e26653476efaa7efac35dbb7b6538ec
Marshall G Gates <mggates@gmail.com>
2023-09-17 01:48:08 -0400

    Strip old and start with new header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='text-center mt-3 list-group-item justify-content-center align-items-center mx-auto'
+            style={{'width':'450px', 'backgroundColor':'#ffffff'}}>
+        <h2 className='card text-white bg-primary mb-2 pb-2'>School Management System</h2>
+      </div>
     </div>
   );
 }
